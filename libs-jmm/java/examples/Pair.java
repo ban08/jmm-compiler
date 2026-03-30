9fe3844fdfb5f9e3921c69ff9b202ea58a0cf5e1
Comp2026 TP Teachers <jbispo@fe.up.pt>
2026-03-30 19:46:36 +0100

    Adds missing example in folder libs-jmm

---
+++ libs-jmm/java/examples/Pair.java
@@ -0,0 +1,19 @@
+package examples;
+
+class Pair {
+    int left;
+    int right;
+
+    public Pair(int left, int right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public int getLeft() {
+        return left;
+    }
+
+    public int getRight() {
+        return right;
+    }
+}
--- libs-jmm/java/util/io.java
+++ libs-jmm/java/util/io.java
@@ -1,4 +1,5 @@
 package util;
+
 import java.io.*;
 import java.lang.*;
 import java.util.*;
@@ -7,51 +8,51 @@ import java.util.*;
 public class io {
 
     // buffer to emulate the buffer of the keyboard
-	static Vector Buffer = new Vector();
-	static boolean init;
+    static Vector<Integer> Buffer = new Vector<Integer>();
+    static boolean init;
 
-	public final static int read() {
-		int a = 0;
+    public final static int read() {
+        int a = 0;
 
-		if(Buffer.size()==0) {
-			init = true;
+        if (Buffer.isEmpty()) {
+            init = true;
 
 
-				BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
-				String line;
-	try {
-				if((line=teclado.readLine()) != null)
-				{
+            BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+            String line;
+            try {
+                if ((line = teclado.readLine()) != null) {
 
-    				String[] tokens = line.split("\\s");
+                    String[] tokens = line.split("\\s");
 
-     				for (int i=0; i<tokens.length; i++) {
-         				//System.out.println(tokens[i]);
+                    for (int i = 0; i < tokens.length; i++) {
+                        //System.out.println(tokens[i]);
 
-						int value=Integer.parseInt(tokens[i]);
+                        int value = Integer.parseInt(tokens[i]);
 
-						//System.out.println("valor lido: "+value);
+                        //System.out.println("valor lido: "+value);
 
-						if(!init) {
-							Buffer.addElement(Integer.valueOf(value));
-						} else {
-							a = value;
-							init = false;
-						}
-						}
-						}
-						} catch (java.io.IOException e) {
-			System.out.println(e);
-			System.exit(1);
-		}
+                        if (!init) {
+                            Buffer.addElement(value);
+                        } else {
+                            a = value;
+                            init = false;
+                        }
+                    }
+                }
+            } catch (java.io.IOException e) {
+//                System.out.println(e);
+                e.printStackTrace(System.err);
+                System.exit(1);
+            }
 
-		} else {
-			Integer top = (Integer) Buffer.firstElement();
-			Buffer.removeElementAt(0);
-			a = top.intValue();
-		}
-		return a;
-	}
+        } else {
+            Integer top = (Integer) Buffer.firstElement();
+            Buffer.removeElementAt(0);
+            a = top;
+        }
+        return a;
+    }
 
 	/* final static int readln() {
 		int a=0;
@@ -65,39 +66,39 @@ public class io {
 		return a;
 	} */
 
-    public final static void print(String c, int a) {
-		System.out.print(c+a);
-	}
+    public static void print(String c, int a) {
+        System.out.print(c + a);
+    }
 
-    public final static void print(int a) {
-		System.out.print(a);
-	}
+    public static void print(int a) {
+        System.out.print(a);
+    }
 
-    public final static void print(String a) {
-		System.out.print(a);
-	}
+    public static void print(String a) {
+        System.out.print(a);
+    }
 
-    public final static void print(boolean a) {
-		System.out.print(a);
-	}
+    public static void print(boolean a) {
+        System.out.print(a);
+    }
 
-    public final static void println() {
-		System.out.println();
-	}
+    public static void println() {
+        System.out.println();
+    }
 
-    public final static void println(String c, int a) {
-		System.out.println(c+a);
-	}
+    public static void println(String c, int a) {
+        System.out.println(c + a);
+    }
 
-    public final static void println(int a) {
-		System.out.println(a);
-	}
+    public static void println(int a) {
+        System.out.println(a);
+    }
 
-    public final static void println(String a) {
-		System.out.println(a);
-	}
+    public static void println(String a) {
+        System.out.println(a);
+    }
 
-    public final static void println(boolean a) {
-		System.out.println(a);
-	}	
+    public static void println(boolean a) {
+        System.out.println(a);
+    }
 }
