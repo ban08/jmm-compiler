d6a8ebd8cdbdb7170cd63497cd25f9d7c35a776c
Filipe Camacho <filipeguerracamacho@gmail.com>
2026-06-03 15:41:13 +0100

    do OLLIR preparation before Jasmin

--- src/main/pt/up/fe/comp2026/backend/JasminBackendImpl.java
+++ src/main/pt/up/fe/comp2026/backend/JasminBackendImpl.java
@@ -1,8 +1,13 @@
 package pt.up.fe.comp2026.backend;
 
+import org.specs.comp.ollir.OllirErrorException;
 import pt.up.fe.comp.jmm.jasmin.JasminBackend;
 import pt.up.fe.comp.jmm.jasmin.JasminResult;
 import pt.up.fe.comp.jmm.ollir.OllirResult;
+import pt.up.fe.comp.jmm.report.Report;
+import pt.up.fe.comp.jmm.report.Stage;
+
+import java.util.ArrayList;
 
 /**
  * Implementation of the Jasmin backend.
@@ -13,6 +18,18 @@ public class JasminBackendImpl implements JasminBackend {
     public JasminResult toJasmin(OllirResult ollirResult) {
 
         //System.out.println("Converting OLLIR to Jasmin:\n" + ollirResult.getOllirCode());
+
+        try {
+            var classUnit = ollirResult.getOllirClass();
+            classUnit.checkMethodLabels();
+            classUnit.buildCFGs();
+            classUnit.buildVarTables();
+        } catch (OllirErrorException e) {
+            var reports = new ArrayList<Report>();
+            reports.add(Report.newError(Stage.BACKEND_GENERATION, -1, -1,
+                    "Could not prepare OLLIR information for Jasmin generation", e));
+            return new JasminResult(ollirResult, "", reports);
+        }
 
         var jasminGenerator = new JasminGenerator(ollirResult);
         var jasminCode = jasminGenerator.build();
